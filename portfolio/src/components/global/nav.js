b2fd54f59ac6cf5616310a5f94e093385d57e188
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-19 23:59:15 -0500

    start work on horizontal nav scroll

--- portfolio/src/components/global/nav.js
+++ portfolio/src/components/global/nav.js
@@ -5,12 +5,12 @@ import './nav.css'
 const Nav = () => {
     return (
         <div id="navbar">
-            <Link to="/#about">About</Link>
-            <Link to="/#pieflix">Pieflix</Link>
-            <Link to="/#robinhoop">Robinhoop</Link>
-            <Link to="/#ask-meeple">Ask Meeple</Link>
-            <Link to="/#verticle-explorer">Verticle Explorer</Link>
-            <Link to="/#contact">Get in Touch</Link>
+            <Link to="about" smooth={true}>About</Link>
+            <Link to="pieflix" smooth={true}>Pieflix</Link>
+            <Link to="robinhoop" smooth={true}>Robinhoop</Link>
+            <Link to="ask-meeple" smooth={true}>Ask Meeple</Link>
+            <Link to="verticle-explorer" smooth={true}>Verticle Explorer</Link>
+            <Link to="contact" smooth={true}>Get in Touch</Link>
         </div>
     )
 }
