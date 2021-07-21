de8a88fb43e14345b8acc043bbd4ec2435b93d20
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-21 17:18:53 -0500

    update splash/landing and about

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -5,9 +5,34 @@ import './about.css'
 const About = () => {
     return(
         <div id="about" className="card">
-            <img src={headshot} alt="Jacob Leonhardt headshot"/>
-            <h3>About</h3>
-            <p>I'm an enthusiastic developer of creative projects. Whether coding an applications, writing stories, or making board games, bringing something from concept to reality is an exciting process; and I appreciate how JavaScript, React, Redux, and CSS allow developers to do this is a visual way.</p>
+            <div className="left">
+                <img src={headshot} alt="Jacob Leonhardt headshot"/>
+            </div>
+            <div className="left">
+                <h3>About</h3>
+                <p>I'm an enthusiastic developer of creative projects. Whether coding an applications, writing stories, or making board games, bringing something from concept to reality is an exciting process! What I love about JavaScript, React, Redux, and CSS, is that they allow developers to do this is a more visual way. Writing backend or frontend code that renders data on screen, then designing the presentation of that data is a creative adventure. And like all adventures, a few obstacles makes the story a little more interesting.</p>
+                <p>On a personal side, I'm a Christian and nerd who drinks too much coffee. My favorite authors are C. S. Lewis, Agatha Christie, and Jane Austen. My favorite music styles are Contemporary Christian, Yacht Rock, and 80s. Favorite video games will be Nintendo. Favorite board games would be Carcassonne, Settlers of Catan, and Star Wars tabletop RPG.</p>
+                <div id="additional">
+                    <h4>Skills</h4>
+                    <i class="devicon-javascript-plain colored"></i>
+                    <i class="devicon-react-original colored"></i>
+                    <i class="devicon-redux-original colored"></i>
+                    <i class="devicon-python-plain colored"></i>
+                    <i class="devicon-flask-original colored"></i>
+                    <i class="devicon-sqlalchemy-original-wordmark colored"></i>
+                    <i class="devicon-postgresql-plain colored"></i>
+                    <i class="devicon-html5-plain colored"></i>
+                    <i class="devicon-css3-plain colored"></i>
+                    <i class="devicon-git-plain colored"></i>
+                    <i class="devicon-ubuntu-plain colored"></i>
+                    <i class="devicon-github-original"></i>
+                    <i class="devicon-vscode-plain colored"></i>
+                    <i class="devicon-photoshop-plain colored"></i>
+                    <i class="devicon-illustrator-plain colored"></i>
+
+                    <button>Resume</button>
+                </div>
+            </div>
         </div>
     )
 }
--- portfolio/src/components/global/nav.js
+++ portfolio/src/components/global/nav.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 import './nav.css'
 
 const Nav = () => {
+
     return (
         // <div id="navbar">
-        //     <Link to="about" smooth={true}>About</Link>
-        //     <Link to="pieflix" smooth={true}>Pieflix</Link>
-        //     <Link to="robinhoop" smooth={true}>Robinhoop</Link>
-        //     <Link to="ask-meeple" smooth={true}>Ask Meeple</Link>
-        //     <Link to="verticle-explorer" smooth={true}>Verticle Explorer</Link>
-        //     <Link to="contact" smooth={true}>Get in Touch</Link>
+        //     <Link to="#about" smooth={true}>About</Link>
+        //     <Link to="#pieflix" smooth={true}>Pieflix</Link>
+        //     <Link to="#robinhoop" smooth={true}>Robinhoop</Link>
+        //     <Link to="#ask-meeple" smooth={true}>Ask Meeple</Link>
+        //     <Link to="#verticle-explorer" smooth={true}>Verticle Explorer</Link>
+        //     <Link to="#contact" smooth={true}>Get in Touch</Link>
         // </div>
         <div id="navbar">
                 <a href="#about" smooth={true}>About</a>
