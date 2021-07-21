5120f9581f36e9b16b1a4072267808027e7701c8
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-21 18:03:37 -0500

    about section reorganized for three div display

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -14,12 +14,17 @@ const About = () => {
             <div className="left">
                 <img src={headshot} alt="Jacob Leonhardt headshot"/>
             </div>
-            <div className="left">
+            <div className="right">
                 <h3>About</h3>
                 <p>I'm an enthusiastic developer of creative projects. Whether coding an applications, writing stories, or making board games, bringing something from concept to reality is an exciting process! What I love about JavaScript, React, Redux, and CSS, is that they allow developers to do this is a more visual way. Writing backend or frontend code that renders data on screen, then designing the presentation of that data is a creative adventure. And like all adventures, a few obstacles makes the story a little more interesting.</p>
-                <p>On a personal side, I'm a Christian and nerd who drinks too much coffee. My favorite authors are C. S. Lewis, Agatha Christie, and Jane Austen. My favorite music styles are Contemporary Christian, Yacht Rock, and 80s. Favorite video games will be Nintendo. Favorite board games would be Carcassonne, Settlers of Catan, and Star Wars tabletop RPG.</p>
-
-                <h4>Skills</h4>
+                <p>On a personal note, I'm a Christian and nerd who drinks a lot of coffee. Some of my favorite things are British Literature, Rend Collective, Yacht Rock, Nintendo, and sarcasm.</p>
+                <span>
+                    <button onClick={viewResume}>Resume</button>
+                    <button>Contact</button>
+                </span>
+            </div>
+            <div className="full">
+            <h4>Skills</h4>
                 <div id="additional">
                     <i class="devicon-javascript-plain colored"></i>
                     <i class="devicon-react-original colored"></i>
@@ -31,14 +36,13 @@ const About = () => {
                     <i class="devicon-html5-plain colored"></i>
                     <i class="devicon-css3-plain colored"></i>
                     <i class="devicon-git-plain colored"></i>
-                    <i class="devicon-ubuntu-plain colored"></i>
                     <i class="devicon-github-original colored"></i>
+                    <i class="devicon-ubuntu-plain colored"></i>
                     <i class="devicon-vscode-plain colored"></i>
+                    <i class="devicon-wordpress-plain"></i>
                     <i class="devicon-photoshop-plain colored"></i>
                     <i class="devicon-illustrator-plain colored"></i>
-                    <i class="devicon-wordpress-plain"></i>
                 </div>
-                    <button onClick={viewResume}>Resume</button> <button>Contact</button>
             </div>
         </div>
     )
