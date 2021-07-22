d32672c48bf8db29874a65a767c80b6123984bba
leonhardt <jacob.leonhardt649@gmail.com>
2021-07-21 20:56:20 -0500

    install and impliment React-Scroll module from github

--- portfolio/src/components/global/nav.js
+++ portfolio/src/components/global/nav.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import * as Scroll from 'react-scroll';
+import { Link, scroll, scrollSpy, scroller } from 'react-scroll'
+// import { NavLink } from 'react-router-dom'
 import './nav.css'
 
 const Nav = () => {
 
+    scroller.scrollTo('scroll-container', {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+    })
+
     return (
-        // <div id="navbar">
-        //     <Link to="#about" smooth={true}>About</Link>
-        //     <Link to="#pieflix" smooth={true}>Pieflix</Link>
-        //     <Link to="#robinhoop" smooth={true}>Robinhoop</Link>
-        //     <Link to="#ask-meeple" smooth={true}>Ask Meeple</Link>
-        //     <Link to="#verticle-explorer" smooth={true}>Verticle Explorer</Link>
-        //     <Link to="#contact" smooth={true}>Get in Touch</Link>
-        // </div>
         <div id="navbar">
-                <a href="#about" smooth={true}>About</a>
-                <a href="#pieflix" smooth={true}>Pieflix</a>
-                <a href="#robinhoop" smooth={true}>Robinhoop</a>
-                <a href="#ask-meeple" smooth={true}>Ask Meeple</a>
-                <a href="#vertical-explorer" smooth={true}>Verticle Explorer</a>
-                <a href="#contact" smooth={true}>Get in Touch</a>
+            <Link to="about" smooth={true} spy={true} duration={800} activeClass="active">About</Link>
+            <Link to="pieflix" smooth={true} spy={true} duration={800} activeClass="active">Pieflix</Link>
+            <Link to="robinhoop" smooth={true} spy={true} duration={800} activeClass="active">Robinhoop</Link>
+            <Link to="vertical-explorer" smooth={true} spy={true} duration={800} activeClass="active">Verticle Explorer</Link>
+            <Link to="ask-meeple" smooth={true} spy={true} duration={800} activeClass="active">Ask Meeple</Link>
+            <Link to="contact" smooth={true} spy={true} duration={800} activeClass="active">Get in Touch</Link>
         </div>
     )
 }
